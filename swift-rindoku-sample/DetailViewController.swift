2fd8628dc1c13ca303f2b012a70860c76a85a7c1
hicka04 <calpissoda8221@gmail.com>
2018-10-04 12:03:13 +0900

    詳細にWebViewを配置

--- swift-rindoku-sample/DetailViewController.swift
+++ swift-rindoku-sample/DetailViewController.swift
@@ -7,29 +7,16 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet private weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.google.co.jp/")!
+        webView.load(URLRequest(url: url))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

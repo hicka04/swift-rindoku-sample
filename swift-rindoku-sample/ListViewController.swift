f8fe1c8e0f71522beefea941f2bebb0d29dbc1d0
hicka04 <calpissoda8221@gmail.com>
2018-10-04 12:03:42 +0900

    一覧に戻ってきたら選択されたセルを非選択状態に変更

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -33,6 +33,14 @@ class ListViewController: UIViewController {
         let nib = UINib(nibName: "RepositoryCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: cellId)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
 }
 
 // UITableViewDelegateとUITableViewDataSourceに準拠

f60e32ebb6f832c72552f61ad9fd16fa75f0f651
hicka04 <calpissoda8221@gmail.com>
2018-10-04 11:21:30 +0900

    配列を元にTableView表示

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -12,6 +12,11 @@ class ListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     let cellId = "cellId"
+    let data = [
+        "hoge",
+        "fuga",
+        "piyo"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +43,13 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // viewDidLoadで登録しておいたセルを取得
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = "hoge"
+        cell.textLabel?.text = data[indexPath.row]
         return cell
     }
     

68d3f7e0c61cf58ee95de0fc8f58a76730aa0594
hicka04 <calpissoda8221@gmail.com>
2019-01-21 16:42:26 +0900

    ブックマーク画面にTableView追加

--- swift-rindoku-sample/BookmarkListViewController.swift
+++ swift-rindoku-sample/BookmarkListViewController.swift
@@ -9,8 +9,34 @@
 import UIKit
 
 class BookmarkListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            tableView.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellReuseIdentifier: "cell")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "bookmarks"
+    }
+}
+
+extension BookmarkListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     }
 }

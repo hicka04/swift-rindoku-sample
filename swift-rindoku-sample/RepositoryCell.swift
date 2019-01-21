5e4424bdff0eb1f6dec3f9d478d80f5f901e7919
hicka04 <calpissoda8221@gmail.com>
2019-01-21 16:44:51 +0900

    リポジトリセルにブックマークボタン追加

--- swift-rindoku-sample/RepositoryCell.swift
+++ swift-rindoku-sample/RepositoryCell.swift
@@ -12,9 +12,20 @@ class RepositoryCell: UITableViewCell {
 
     // カスタムViewの中に置いているSubViewはprivateにするのがおすすめ
     // 外から触れるとどこで見た目の変化を行っているのかの影響範囲が広くなってしまうため
-    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var repositoryNameLabel: UILabel!
+    @IBOutlet private weak var bookmarkButton: UIButton!
+    
+    private var isBookmarked: Bool = false {
+        didSet {
+            bookmarkButton.setImage(isBookmarked ? #imageLiteral(resourceName: "bookmark") : #imageLiteral(resourceName: "bookmark_border"), for: .normal)
+        }
+    }
+    
+    @IBAction private func bookmarkButtonDidTap() {
+        isBookmarked.toggle()
+    }
     
     func set(repositoryName name: String) {
-        label.text = name
+        repositoryNameLabel.text = name
     }
 }

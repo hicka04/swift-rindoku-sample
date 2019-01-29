4176f965a3db3afd183025441a7bc0a54bf50589
hicka04 <calpissoda8221@gmail.com>
2019-01-29 21:24:13 +0900

    ブックマークに追加されたらタブにバッヂを表示する処理追加

--- swift-rindoku-sample/TabBarController.swift
+++ swift-rindoku-sample/TabBarController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TabBarController: UITabBarController {
+    
+    private var bookmarkUpdateToken: NotificationToken?
+    private var badgeCount: Int = 0 {
+        didSet {
+            guard badgeCount > 0 else {
+                bookmark.tabBarItem.badgeValue = nil
+                badgeCount = 0
+                return
+            }
+            
+            bookmark.tabBarItem.badgeValue = "\(badgeCount)"
+        }
+    }
+    
+    let searchResult: UIViewController = {
+        let searchResult = UINavigationController(rootViewController: SearchResultListViewController())
+        searchResult.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return searchResult
+    }()
+    
+    let bookmark: UIViewController = {
+        let bookmark = UINavigationController(rootViewController: BookmarkListViewController())
+        bookmark.tabBarItem = UITabBarItem(title: "bookmarks", image: #imageLiteral(resourceName: "bookmarks"), tag: 1)
+        return bookmark
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let searchResult = UINavigationController(rootViewController: SearchResultListViewController())
-        searchResult.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        let bookmark = UINavigationController(rootViewController: BookmarkListViewController())
-        bookmark.tabBarItem = UITabBarItem(title: "bookmarks", image: #imageLiteral(resourceName: "bookmarks"), tag: 1)
+        
         viewControllers = [
             searchResult,
             bookmark
         ]
+        
+        bookmarkUpdateToken = try! Realm().objects(Bookmark.self).observe({ [weak self] change in
+            guard let `self` = self else { return }
+            
+            switch change {
+            case .update(_, let deletions, let insertions, _):
+                self.badgeCount += insertions.count - deletions.count
+            default:
+                break
+            }
+        })
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard item == bookmark.tabBarItem else {
+            return
+        }
+        
+        badgeCount = 0
     }
 }

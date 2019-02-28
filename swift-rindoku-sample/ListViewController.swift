ea4384a2e9babac744c4cc36b553ce9119820d9e
HikaruSato <hicka04@gmail.com>
2019-02-28 11:19:21 +0900

    Revert "Feat/bookmark"

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SearchResultListViewController.swift
+//  ViewController.swift
 //  swift-rindoku-sample
 //
 //  Created by hicka04 on 2018/08/11.
@@ -7,19 +7,14 @@
 //
 
 import UIKit
-import RealmSwift
 
-class SearchResultListViewController: UIViewController {
+class ListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     private lazy var searchController: UISearchController = {
-        let resultsController = SearchKeywordHistoryListViewController()
-        resultsController.delegate = self
-        let searchController = UISearchController(searchResultsController: resultsController)
-        searchController.searchResultsUpdater = resultsController
+        let searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "キーワードを入力"
         searchController.searchBar.delegate = self
-        searchController.delegate = self
         return searchController
     }()
     
@@ -39,46 +34,14 @@ class SearchResultListViewController: UIViewController {
                     }
                 }
             }
-            
-            if searchController.searchBar.text?.isEmpty ?? true {
-                searchController.searchBar.text = keyword
-            }
-            
-            if let sameKeywordHistory = realm.objects(SearchKeywordHistory.self).first(where: { $0.keyword == keyword }) {
-                try! realm.write {
-                    sameKeywordHistory.lastSearchAt = Date()
-                }
-            } else {
-                let max = 50
-                if realm.objects(SearchKeywordHistory.self).count >= max {
-                    realm.objects(SearchKeywordHistory.self).sorted(byKeyPath: "lastSearchAt").enumerated().forEach { (offset, history) in
-                        guard offset >= max - 1 else {
-                            return
-                        }
-                        try! realm.write {
-                            realm.delete(history)
-                        }
-                    }
-                }
-                let history = SearchKeywordHistory()
-                history.keyword = keyword
-                try! realm.write {
-                    realm.add(history)
-                }
-            }
         }
     }
-    
-    let realm = try! Realm()
-    
     // 配列を定義してこれを元にtableViewに表示
     // APIクライアントを作ったらそのデータに差し替え
     var data: [Repository] = [] {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
-                
-                self.tableView.flashScrollIndicators()
             }
         }
     }
@@ -105,12 +68,6 @@ class SearchResultListViewController: UIViewController {
         
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-        
-        if let last = realm.objects(SearchKeywordHistory.self).sorted(byKeyPath: "lastSearchAt").last {
-            keyword = last.keyword
-        }
-        
-        print(realm.objects(SearchKeywordHistory.self))
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -126,7 +83,7 @@ class SearchResultListViewController: UIViewController {
 
 // UITableViewDelegateとUITableViewDataSourceに準拠
 // extensionに切り出すと可読性が上がる
-extension SearchResultListViewController: UITableViewDelegate, UITableViewDataSource {
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -141,7 +98,7 @@ extension SearchResultListViewController: UITableViewDelegate, UITableViewDataSo
         // viewDidLoadで登録しておいたセルを取得
         // カスタムセルを取り出すときはキャストが必要(強制案ラップでOK)
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! RepositoryCell
-        cell.set(repository: data[indexPath.row], delegate: self)
+        cell.set(repositoryName: data[indexPath.row].fullName)
         return cell
     }
     
@@ -150,8 +107,7 @@ extension SearchResultListViewController: UITableViewDelegate, UITableViewDataSo
         // 押されたセルの場所(indexPath)などに応じて処理を変えることができるが
         // 今回は必ずDetailViewControllerに遷移するように実装
         let repository = data[indexPath.row]
-        let detailView = RepositoryDetailViewController(repository: repository)
-        detailView.hidesBottomBarWhenPushed = true
+        let detailView = DetailViewController(repository: repository)
         
         // navigationController:画面遷移を司るクラス
         // pushViewController(_:animated:)で画面遷移できる
@@ -159,7 +115,7 @@ extension SearchResultListViewController: UITableViewDelegate, UITableViewDataSo
     }
 }
 
-extension SearchResultListViewController: UISearchBarDelegate {
+extension ListViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         defer {
@@ -173,51 +129,5 @@ extension SearchResultListViewController: UISearchBarDelegate {
         keyword = searchBarText
         
         tableView.setContentOffset(.zero, animated: true)
-    }
-}
-
-extension SearchResultListViewController: UISearchControllerDelegate {
-    
-    func didDismissSearchController(_ searchController: UISearchController) {
-        searchController.searchBar.text = keyword
-    }
-}
-
-extension SearchResultListViewController: SearchResultsControllerDelegate {
-    
-    func resultsController(_ resultsController: UIViewController,
-                           didUpdateKeyword keyword: String,
-                           shouldSearch: Bool) {
-        searchController.searchBar.text = keyword
-        if shouldSearch {
-            self.keyword = keyword
-            searchController.dismiss(animated: true, completion: nil)
-            tableView.setContentOffset(.zero, animated: true)
-        }
-    }
-}
-
-extension SearchResultListViewController: RepositoryCellDelegate {
-    
-    func repositoryCell(_ cell: RepositoryCell, didTapBookmarkButtonFrom repository: Repository) {
-        if realm.objects(Bookmark.self).contains(where: { bookmark in bookmark.repository.id == repository.id }) {
-            realm.objects(Bookmark.self)
-                .filter { bookmark -> Bool in
-                    bookmark.repository.id == repository.id
-                }.forEach { bookmark in
-                    try! realm.write {
-                        realm.delete(bookmark)
-                    }
-                }
-        } else {
-            guard realm.objects(Bookmark.self).count < 50 else {
-                present(UIAlertController.createBookmarkLimitAlert(), animated: true, completion: nil)
-                return
-            }
-            
-            try! realm.write {
-                realm.add(Bookmark(repository: repository))
-            }
-        }
     }
 }
